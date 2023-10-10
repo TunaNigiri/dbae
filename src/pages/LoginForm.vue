c7e7c08ff3ac8b5160ea5389bccdc5ef73cb018f
TunaNigiri <antuelbolson@gmail.com>
2023-10-10 11:42:31 -0300

    First real commit

---
+++ src/pages/LoginForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="wrapper">
+      <h3 class="flex flex-center">DBAE</h3>
+        <q-form>
+          
+          <div class="flex flex-row flex-center">
+            <q-icon size="lg"  name="person"/>
+            <q-input class="q-pa-none" rounded outlined v-model="text" label="" />
+          </div>
+          <div class="flex flex-row flex-center">
+            <q-icon size="lg"  name="key"/>
+            <q-input rounded outlined v-model="text" label="" />
+          </div>
+
+          <q-btn>
+            
+          </q-btn>
+
+        </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+import { defineAsyncComponent} from 'vue';
+
+    export default {
+        
+        data() {
+            return {
+                password: "1234",
+                userName: "Alberto",
+                loginButton: "Login",
+                disableButton: false,
+                inputName: "",
+                inputPassword: "",
+                popUpColor: "",
+                popUpText: ""
+            }
+        },
+        methods: {
+            login() {
+                this.disableButton = true
+                if (this.inputName === this.userName && this.inputPassword === this.password){
+                    this.loginButton = "Loading"
+                    this.popUpColor = "green"
+                    this.popUpText = "Bienvenido"
+                }
+                else {
+                   
+                    this.disableButton = false
+                    this.inputName = ""
+                    this.inputPassword = ""
+                    this.popUpColor = "red"
+                    this.popUpText = "Login incorrecto"
+                }           
+            }
+        },
+        components: {
+            // PopUp: defineAsyncComponent(() => import ('./PopUp.vue'))
+        }
+    }
+
+
+</script>
+
+<style>
+
+.q-placeholder{
+  padding:0 !important;
+}
+
+</style>
